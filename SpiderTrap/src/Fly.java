420a6c9911ded089e24c9cdf04b934b1aff78c6b
Paula Navarro <pteriwinkle@gmail.com>
2014-12-03 14:29:56 -0800

    initial commit

---
+++ SpiderTrap/src/Fly.java
@@ -0,0 +1,13 @@
+
+public class Fly {
+	static final int FLY_RADIUS = 10; //so view can call on this 
+	//add wings here? maybe later
+	int x, y; //left right point of circle 
+	
+	public Fly(int x, int y) {
+		this.x = x; 
+		this.y = y; 
+	}
+	
+	//I don't think I need anything else
+}
---
+++ SpiderTrap/src/SpiderTrapController.java
@@ -0,0 +1,41 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+public class SpiderTrapController implements MouseListener{
+	//timer in controller: tell model to update positions, give new positions
+	//controller --> model --> view 
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		//model.something
+		//view.something
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
